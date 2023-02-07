ba1133d045b907f832c470ee48a1b9ecb1f2f07d
Joshualauw <joshualauw1@gmail.com>
2023-02-07 23:18:43 +0700

    finishing

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCCFYtgvIRSbe_AIyiaBsIRWv9wSEHJvLA",
-    authDomain: "svelte-expense-tracker-e21bf.firebaseapp.com",
-    projectId: "svelte-expense-tracker-e21bf",
-    storageBucket: "svelte-expense-tracker-e21bf.appspot.com",
-    messagingSenderId: "746777814448",
-    appId: "1:746777814448:web:c1f2eb577706bb38fc2275",
+    apiKey: import.meta.env.FIREBASE_API_KEY,
+    authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.FIREBASE_APP_ID,
 };
 
 initializeApp(firebaseConfig);
